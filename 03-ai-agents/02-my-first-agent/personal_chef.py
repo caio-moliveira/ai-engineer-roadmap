da7c2dd463095b7a32699240204db91daf89ad10
Caio Machado <moliveiracaio@gmail.com>
2026-03-15 17:12:42 -0300

    first agent

--- 03-ai-agents/02-my-first-agent/personal_chef.py
+++ 03-ai-agents/02-my-first-agent/personal_chef.py
@@ -1,34 +1,54 @@
 from dotenv import load_dotenv
+from typing import List
+from pydantic import BaseModel, Field
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage
+from pprint import pprint
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-from langchain.tools import tool
-from typing import Dict, Any
-from tavily import TavilyClient
+tavily_search_tool = TavilySearch(
+    max_results=5,
+    topic="general",
+)
 
-tavily_client = TavilyClient()
+class Recipe(BaseModel):
+    nome: str = Field(description="Nome da receita")
+    ingredientes: List[str] = Field(description="Lista de ingredientes principais")
+    instrucoes: str = Field(description="Modo de preparo resumido")
 
-@tool
-def web_search(query: str) -> Dict[str, Any]:
-
-    """Search the web for information"""
-
-    return tavily_client.search(query)
+class RecipeList(BaseModel):
+    receitas: List[Recipe]
 
 system_prompt = """
+Você é um chef pessoal.
+O usuário fornecerá uma lista de ingredientes que sobraram em casa.
 
-You are a personal chef. The user will give you a list of ingredients they have left over in their house.
+Sua tarefa é:
+1. Usar a ferramenta de busca na web quando necessário.
+2. Encontrar receitas compatíveis com os ingredientes disponíveis.
+3. Sugerir receitas simples e realistas.
+4. Responder sempre no formato estruturado solicitado.
 
-Using the web search tool, search the web for recipes that can be made with the ingredients they have.
-
-Return recipe suggestions and eventually the recipe instructions to the user, if requested.
-
+Se nem todos os ingredientes forem usados, tudo bem.
+Priorize receitas fáceis e comuns.
 """
 
-from langchain.agents import create_agent
-
 agent = create_agent(
-    model="gpt-5-nano",
-    tools=[web_search],
-    system_prompt=system_prompt
+    model="openai:gpt-5",  # ajuste para o modelo/provider que você tiver disponível
+    tools=[tavily_search_tool],
+    system_prompt=system_prompt,
+    response_format=RecipeList
 )
+
+question = HumanMessage(
+    content="Tenho frango, arroz, feijão e batata. O que posso fazer?"
+)
+
+
+for step in agent.stream(
+    {"messages": question},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
